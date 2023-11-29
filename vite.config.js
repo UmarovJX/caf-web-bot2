ab36704228a7ab54f266a8681034e83c186ebbf3
umarovjx <umjahongir@gmail.com>
2023-11-29 12:54:00 +0500

    asd

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     VueI18nPlugin({
       include: resolve(
         dirname(fileURLToPath(import.meta.url)),
-        "./src/locales"
+        "./src/locales/**"
       ),
       //resolve(dirname(fileURLToPath(import.meta.url)), "./"),
       runtimeOnly: false,
