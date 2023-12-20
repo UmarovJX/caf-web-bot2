82509cd0f15e3eee58677880d8fe764d86ab72e7
umarovjx <umjahongir@gmail.com>
2023-12-20 22:54:05 +0500

    modal backdrop

--- src/components/elements/drawer/AppDrawer.vue
+++ src/components/elements/drawer/AppDrawer.vue
@@ -37,25 +37,17 @@ onMounted(() => {
   <Teleport :disabled="props.show" to="body">
     <Transition name="modal">
       <div v-if="props.show" class="modal-mask">
-        <div
-          style="
-            width: 100%;
-            height: 100vh;
-            background-color: rgba(0, 0, 0, 0.5);
-          "
-        >
-          <div class="modal-container">
-            <div class="modal-header">
-              <slot name="header"></slot>
-            </div>
+        <div class="modal-container">
+          <div class="modal-header">
+            <slot name="header"></slot>
+          </div>
 
-            <div class="modal-body">
-              <slot name="body"></slot>
-            </div>
+          <div class="modal-body">
+            <slot name="body"></slot>
+          </div>
 
-            <div class="modal-footer">
-              <slot name="footer"></slot>
-            </div>
+          <div class="modal-footer">
+            <slot name="footer"></slot>
           </div>
         </div>
       </div>
@@ -91,13 +83,17 @@ onMounted(() => {
   z-index: 9998;
   bottom: 0;
   width: 100%;
+  height: 100vh;
   margin: auto;
-  background-color: var(--background_secondary);
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: end;
   transition: all 0.5s ease-in-out;
   border-radius: 16px 16px 0 0;
   border: 1px solid var(--background_secondary);
   overflow: hidden;
-  //opacity: 1;
 }
 
 //.modal-enter-from {
