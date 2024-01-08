5ed701d041ffdc65c989c94885a0d733c8be6aac
umarovjx <umjahongir@gmail.com>
2024-01-08 19:17:42 +0500

    dsda

--- src/composable/client.js
+++ src/composable/client.js
@@ -4,11 +4,15 @@ import api from "@/services/api";
 const user = ref(null);
 
 export async function useUser() {
-  if (!user.value) {
-    const {
-      data: { result },
-    } = await api.client.getClient();
-    user.value = result;
+  try {
+    if (!user.value) {
+      const {
+        data: { result },
+      } = await api.client.getClient();
+      user.value = result;
+    }
+  } catch (error) {
+    alert(error);
   }
   return {
     user,
