69390701fda56ce3bde881c582b43e444168e6e3
umarovjx <umjahongir@gmail.com>
2024-01-08 18:12:38 +0500

    www

--- src/main.js
+++ src/main.js
@@ -7,9 +7,6 @@ import { i18n } from "@/locales";
 
 import "./assets/scss/main.scss";
 window.onunhandledrejection = function (e) {
-  // Handle this case... For example I can send info
-  // about this error to developer's email.
-  window.Telegram.WebApp.showAlert(e.type);
   window.Telegram.WebApp.showAlert(e.reason.message);
 };
 const app = createApp(App);
