8e93a7259f000ea10540c6dd81a4277d2543f821
umarovjx <umjahongir@gmail.com>
2024-01-08 18:09:59 +0500

    ddd

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import router from "./router";
 import { i18n } from "@/locales";
 
 import "./assets/scss/main.scss";
-
+window.onunhandledrejection = function (e) {
+  // Handle this case... For example I can send info
+  // about this error to developer's email.
+  window.Telegram.WebApp.showAlert(e.type);
+  window.Telegram.WebApp.showAlert(e.reason.message);
+};
 const app = createApp(App);
 
 app.use(createPinia());
