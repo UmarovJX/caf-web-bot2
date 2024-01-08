78d796d861a1b04b9a8919ae94385911ae62f242
umarovjx <umjahongir@gmail.com>
2024-01-08 18:18:47 +0500

    debug

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { i18n } from "@/locales";
 
 import "./assets/scss/main.scss";
 window.onunhandledrejection = function (e) {
-  window.Telegram.WebApp.showAlert(e.reason.message);
+  window.Telegram.WebApp.showAlert(Object.keys(e).join(","));
 };
 const app = createApp(App);
 
